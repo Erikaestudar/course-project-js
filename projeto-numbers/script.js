56948a74c261dac2c056673196f2da5d1ce28834
Erika Miyuki Sugawara <erika_cefam@hotmail.com>
2025-08-06 17:19:32 -0300

    construindo o estilo das animações.

--- projeto-numbers/script.js
+++ projeto-numbers/script.js
@@ -1,16 +1,50 @@
 let form = document.querySelector("form")
 
-let quantity = document.querySelector("#quantity")
+let number = document.querySelector(".number")
+
+let inputs = document.querySelectorAll('input[type="text"]')
+
+let result = document.querySelector(".result")
+
+inputs.forEach((input) => {
+    input.addEventListener("input", () => {
+    
+    let valueRegex = /\D+/g
+    input.value = input.value.replace(valueRegex, "")
 
 
+    let numbersWrapper = document.createElement("div")
+    numbersWrapper.classList.add("numbers-wrapper")
+
+    let square = document.createElement("div")
+    square.classList.add("square")
+
+    let number = document.createElement("span")
+    number.classList.add("number")
+    number.textContent = input.value
+        
+    numbersWrapper.append(square, number)
+    result.append(numbersWrapper)
+    })
+
+    
+
+})
 
 form.addEventListener("submit", (event) => {
     event.defaultPrevented()
 
-    
+
+
+console.log(result)
 })
 
+
+
+/*
 quantity.addEventListener("input", (event) => {
     let num = quantity.value
     console.log(num)
 })
+    */
+
